1930ef581e8945f1e55a229a0822771a2fb2ee27
thinkercards <85484853+thinkercards@users.noreply.github.com>
2021-07-24 00:47:24 +0530

    Create Vanilla JavaScript Snippet Collection Two
    Vanilla JavaScript Snippet Collection Two

---
+++ VanillaJavaScriptSnippetTwo.js
@@ -0,0 +1,119 @@
+/*Vanilla JS equivalents of jQuery methods */
+/*
+Selectors
+*/
+// jQuery
+var divs = $('div')
+
+// Vanilla
+var divs = document.querySelectorAll('div')
+// jQuery
+var newDiv = $('<div/>')
+
+// Vanilla
+var newDiv = document.createElement('div')
+Attributes
+// jQuery
+$('img').filter(':first').attr('alt', 'My image')
+
+// Vanilla
+document.querySelector('img').setAttribute('alt', 'My image')
+Classes
+// jQuery
+newDiv.addClass('foo')
+
+// Vanilla
+newDiv.classList.add('foo')
+// jQuery
+newDiv.toggleClass('foo')
+
+// Vanilla
+newDiv.classList.toggle('foo')
+
+/*
+Manipulation
+*/
+
+// jQuery
+$('body').append($('<p/>'))
+
+// Vanilla
+document.body.appendChild(document.createElement('p'))
+// jQuery
+var clonedElement = $('#about').clone()
+
+// Vanilla
+var clonedElement = document.getElementById('about').cloneNode(true)
+// jQuery
+$('#wrap').empty()
+
+// Vanilla
+var wrap = document.getElementById('wrap')
+while(wrap.firstChild) wrap.removeChild(wrap.firstChild)
+
+/*
+Transversing
+*/
+
+// jQuery
+var parent = $('#about').parent()
+
+// Vanilla
+var parent = document.getElementById('about').parentNode
+// jQuery
+if($('#wrap').is(':empty'))
+
+// Vanilla
+if(!document.getElementById('wrap').hasChildNodes())
+// jQuery
+var nextElement = $('#wrap').next()
+
+// Vanilla
+var nextElement = document.getElementById('wrap').nextSibling
+/*
+AJAX
+GET
+*/
+// jQuery
+$.get('//example.com', function (data) {
+  // code
+})
+
+// Vanilla
+var httpRequest = new XMLHttpRequest()
+httpRequest.onreadystatechange = function (data) {
+  // code
+}
+httpRequest.open('GET', url)
+httpRequest.send()
+
+/*
+POST
+*/
+
+// jQuery
+$.post('//example.com', { username: username }, function (data) {
+  // code
+})
+
+// Vanilla
+var httpRequest = new XMLHttpRequest()
+httpRequest.onreadystatechange = function (data) {
+  // code
+}
+httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+httpRequest.open('POST', url)
+httpRequest.send('username=' + encodeURIComponent(username))
+JSONP
+// jQuery
+$.getJSON('//openexchangerates.org/latest.json?callback=?', function (data) {
+  // code
+})
+
+// Vanilla
+function success(data) {
+  // code
+}
+var scr = document.createElement('script')
+scr.src = '//openexchangerates.org/latest.json?callback=formatCurrency'
+document.body.appendChild(scr)
